50bcf3cf842f8548773eb84b5b55406597c09788
Sakaowrat Suwitchayasiri <s.suwitchayasiri@gmail.com>
2022-01-19 20:52:50 +0700

    blogapp

---
+++ blogapp/models.py
@@ -0,0 +1,13 @@
+from distutils.command.upload import upload
+from pickle import TRUE
+from tokenize import blank_re
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="appImage",blank=TRUE)
+    body = models.TextField()
+    
+    def __str__(self):
+        return self.title
